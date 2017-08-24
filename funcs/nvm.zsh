d36d2e4de3b343f698b34b63fd0d5d44edfc79a5
Brian Devins <badevins@gmail.com>
2017-08-23 21:13:30 -0400

    Stil trying to fix nvm

--- funcs/nvm.zsh
+++ funcs/nvm.zsh
@@ -1,6 +1,6 @@
 function __devinsba_theme_nvm() {
     if [ -n "$(whence -v nvm_version)" ] ; then
-        nvm_version=$(command nvm_version) || return 0
-        echo "nvm(${nvm_version})"
+        node_version=$(nvm_version) || return 0
+        echo "nvm(${node_version})"
     fi
 }
--- funcs/rvm.zsh
+++ funcs/rvm.zsh
@@ -1,6 +1,6 @@
 function __devinsba_theme_rvm() {
     if [ -f ~/.rvm/bin/rvm-prompt ] ; then
-        rvm_version=$(command ~/.rvm/bin/rvm-prompt) || return 0
-        echo "rvm(${rvm_version})"
+        ruby_version=$(command ~/.rvm/bin/rvm-prompt) || return 0
+        echo "rvm(${ruby_version})"
     fi
 }
