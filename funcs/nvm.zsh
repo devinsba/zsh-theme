0f20916d28ff102daeaf9ba9560fed54c1885a11
Brian Devins <badevins@gmail.com>
2017-08-23 21:04:36 -0400

    Fix error with nvm

--- funcs/nvm.zsh
+++ funcs/nvm.zsh
@@ -1,6 +1,6 @@
 function __devinsba_theme_nvm() {
-    if [ -n "$(type -t nvm_prompt_info)" ] ; then
-        nvm_version=$(command nvm_prompt_info) || return 0
+    if [ -n "$(whence -v nvm_version)" ] ; then
+        nvm_version=$(command nvm_version) || return 0
         echo "nvm(${nvm_version})"
     fi
 }
